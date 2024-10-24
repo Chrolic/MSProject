43c22e58340cd6582638e0d9241b1b7b3f4cc6d0
Vilster <Vilster@AWESOME-PC>
2024-10-24 19:47:08 +0200

    Added test for pizzaria controller endpoint placeorder.

---
+++ UnitTests/UnitTests/Pizzaria/PizzariaTests.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Pizzaria.Controllers;
+using Pizzaria.Services.Interfaces;
+using Pizzaria.Utilities.DTOs;
+
+namespace UnitTests.UnitTests.Pizzaria
+{
+    public class PizzariaTests
+    {
+        private Mock<IPizzariaService> _pizzariaService;
+
+        public PizzariaTests()
+        {
+            _pizzariaService = new Mock<IPizzariaService>();
+        }
+
+        [Fact]
+        public async void TestPizzariaService()
+        {
+            // Test pizzaria controller response using Nuget 'Moq'.
+
+            // ------- ARRANGE -------
+            var cancellationTokenSource = new CancellationTokenSource();
+            var pizzariaController = new PizzariaController(_pizzariaService.Object);
+
+            var createOrderDto = new CreateOrderDto
+            {
+                CustomerName = "Carsten Carstensen",
+                Address = "Generisk vej 1",
+                PhoneNumber = "12345678",
+                Table = "12B",
+                PizzaMenuNumber = 9
+            };
+
+            var returnDto = new ReadEventDto
+            {
+                SequenceNumber = 1,
+                OccuredAt = DateTimeOffset.UtcNow,
+                Name = createOrderDto.CustomerName,
+                Content = new
+                {
+                    CustomerName = createOrderDto.CustomerName,
+                    Address = createOrderDto.Address,
+                    PhoneNumber = createOrderDto.PhoneNumber,
+                    Table = createOrderDto.Table,
+                    PizzaMenuNumber = createOrderDto.PizzaMenuNumber
+                }
+            };
+
+            _pizzariaService.Setup(x => x.PlaceOrder(createOrderDto, cancellationTokenSource.Token)).ReturnsAsync(returnDto);
+
+
+            // ------- ACT -------
+            var result = await pizzariaController.PlaceOrder(createOrderDto, cancellationTokenSource.Token);
+
+            // Create result object to assert on
+            var resultObject = result as OkObjectResult;
+
+
+            // ------- ASSERT -------
+            Assert.NotNull(resultObject);
+            Assert.IsType<OkObjectResult>(resultObject);
+            Assert.Equal(200, resultObject.StatusCode);
+            Assert.Equal(returnDto, resultObject.Value);
+        }
+    }
+}
