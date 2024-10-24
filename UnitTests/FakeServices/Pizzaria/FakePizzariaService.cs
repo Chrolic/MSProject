c5580045095139bddac1af69210d18903a1b8b05
Vilster <Vilster@AWESOME-PC>
2024-10-24 15:05:53 +0200

    Added initial unit test for pizzaria controller endpoint.

---
+++ UnitTests/FakeServices/Pizzaria/FakePizzariaService.cs
@@ -0,0 +1,28 @@
+﻿using Pizzaria.Services.Interfaces;
+using Pizzaria.Utilities.DTOs;
+
+namespace Pizzaria.UnitTests.FakeServices
+{
+    public class FakePizzariaService : IPizzariaService
+    {
+        public async Task<ReadEventDto> PlaceOrder(CreateOrderDto dto, CancellationToken cancellationToken)
+        {
+            var returnDto = new ReadEventDto
+            {
+                SequenceNumber = 1,
+                OccuredAt = DateTimeOffset.UtcNow,
+                Name = dto.CustomerName,
+                Content = new
+                {
+                    CustomerName = dto.CustomerName,
+		            Address =  dto.Address,
+		            PhoneNumber = dto.PhoneNumber,
+		            Table = dto.Table,
+		            PizzaMenuNumber = dto.PizzaMenuNumber
+                }
+            };
+      
+            return returnDto;
+        }
+    }
+}
